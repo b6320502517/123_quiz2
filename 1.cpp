5598f25f00eb45a9a9ca3322eb4bf7577ff3f499
b6320502517 <sarannut.l@ku.th>
2021-03-25 17:37:43 +0700

    1

--- 1.cpp
+++ 1.cpp
@@ -1,25 +1,27 @@
 #include<stdio.h>
 int main(){
-	unsigned long n=1;
-	int i,j,k=1,l=0;
+	unsigned long n=1,i,j,x;
+	int k=1,l=0;
 	if(n>=1&&n<=1000000)
 		scanf("%ld",&n);
 	if(n==1)
 		printf("1");
 	for(i=2;i<=n;i++){
-		for(j=2;i!=1;j++){
+		x=i;
+		for(j=2;x!=1;j++){
 			l=0;
-			while(i%j==0){
+			while(x%j==0){
 				l++;
-				i/=j;	
+				x/=j;	
 			}
 		}
-		printf("%d",l);
-			if(l==3)
-			k--;
-			else
-			k++;
+		//printf("%d",l);
+			if(l!=3)
+				k++;
+
+			//printf("%ld\n",i);
 	}
+	i--;
 	if(i==n&&l==3)
 		printf("Not Cube Free");
 	else
