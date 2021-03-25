b30e9b8881d53ec03eec54b6230f7e112b77af33
b6320502517 <sarannut.l@ku.th>
2021-03-25 18:46:28 +0700

    1.1

---
+++ 1.1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	unsigned long n=1,i,j,x,k=1,l=0;
+	if(n>=1&&n<=1000000)
+		scanf("%ld",&n);
+	if(n==1)
+		printf("1");
+	for(i=2;i<=n;i++){
+		x=i;
+		for(j=2;x!=1;j++){
+			l=0;
+			while(x%j==0){
+				l++;
+				x/=j;	
+			}
+		}
+		if(l!=3)
+			k++;
+	}
+	i--;
+	if(i==n&&l==3)
+		printf("Not Cube Free");
+	else
+		printf("%ld",k);
+	
+	return 0;
+}
