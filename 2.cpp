6d80d44a646c19be73186caf6fe6cf2de326d984
b6320502517 <sarannut.l@ku.th>
2021-03-25 19:27:44 +0700

    2

--- 2.cpp
+++ 2.cpp
@@ -1,4 +1,13 @@
 #include<stdio.h>
 int main(){
+	long n=1,m=1,i,j,l;
+	if(m>=1&&m<=50000)
+		scanf("%ld",&m);
+	if(n>=1&&n<=5000)
+		scanf("%ld",&n);	
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++)
+			scanf("%ld",&l);
+	}
 	return 0;
 }
